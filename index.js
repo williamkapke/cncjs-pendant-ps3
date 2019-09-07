9d9d0076c1454a6136ffdbb2fa70a818640334b9
Garth Vander Houwen <garthvh@yahoo.com>
2019-09-07 12:41:57 -0700

    logs

--- index.js
+++ index.js
@@ -422,9 +422,9 @@ module.exports = function(options, callback) {
 		// M7 or Custom mapped command
 		controller.on('triangle:press', function(data) {
 			if (psx) {
-				console.log(config.customButtonMapping);
+				console.log(config.hasOwnProperty('psxTriangle'));
 				console.log(config.psxTriangle);
-				if(config.customButtonMapping && config.hasOwnProperty('psxTriangle')){
+				if(config.hasOwnProperty('psxTriangle')){
 					console.log("Trying to send custom command: " + config.psxTriangle)
 					socket.emit('command', options.port, 'gcode', config.psxTriangle);
 				}
